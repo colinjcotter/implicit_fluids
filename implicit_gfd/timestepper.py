1ebad7dc0e00e539e5133992453df5df9b9aa270
Colin Cotter <colin.cotter@imperial.ac.uk>
2025-11-28 19:36:47 +0000

    tiding up

--- implicit_gfd/timestepper.py
+++ implicit_gfd/timestepper.py
@@ -7,17 +7,17 @@ from firedrake import ProgressBar
 opts = PETSc.Options()
 print = PETSc.Sys.Print
 
-model_opts = PETSc.Options("model_")
+model_opts = PETSc.Options('model_')
 model = get_model(model_opts)
 
 dt = opts.getScalar('model_dt', 1.0)
 tmax = opts.getScalar('model_tmax', 1.0)
 nsteps = int(tmax/dt)
-assert fabs(nsteps*dt - tmax) < 1.0e-6*dt
+assert fabs(nsteps*dt - tmax) < 1.0e-6*dt, 'tmax is not a multiple of dt'
 
-stepper_opts = PETSc.Options("stepper_")
+stepper_opts = PETSc.Options('stepper_')
 stepper, dt, t = get_stepper(model, stepper_opts)
 
-for step in ProgressBar("Timestep").iter(range(nsteps)):
+for step in ProgressBar('Timestep').iter(range(nsteps)):
     stepper.advance()
     t.assign(float(t) + float(dt))
