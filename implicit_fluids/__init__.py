ce57448e848693afad24578d1601dada477748d9
Colin Cotter <colin.cotter@imperial.ac.uk>
2026-01-16 14:23:52 +0000

    init

---
+++ implicit_fluids/__init__.py
@@ -0,0 +1,4 @@
+from implicit_fluids.models import *  # noqa: F401
+from implicit_fluids.testcases import *  # noqa: F401
+from implicit_fluids.steppers import *  # noqa: F401
+from implicit_fluids.timestepper import *  # noqa: F401
